3123438fe4526508ee75c747f803cfd2f80447e2
William Han Findiesen <william.findiesen@gmail.com>
2020-06-27 20:33:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,10 +6,4 @@ def square_array(array)
  arr = [1,2,3]
 square_array(arr)
   
-  def square_array(array)
-  new_array = []
-  array.each{|a| new_array.push(a*a)}
-  return new_array
-end
-arr = [1, 2, 3, 4, 5]
-square_array(arr)
+ 
